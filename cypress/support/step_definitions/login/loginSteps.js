a25059cf6495c88eb4bac939a8f14666abaeb878
Sebastian Szemer <sebastian.szemer@esky.com>
2022-09-25 14:43:29 +0200

    bdd

---
+++ cypress/support/step_definitions/login/loginSteps.js
@@ -0,0 +1,28 @@
+import {
+	Given,
+	When,
+	Then,
+	And,
+} from '@badeball/cypress-cucumber-preprocessor';
+
+import LoginPage from '../../../pageObjects/loginPage';
+import MainPage from '../../../pageObjects/mainPage';
+
+const loginPage = new LoginPage();
+const mainPage = new MainPage();
+
+Given('I open login page', () => {
+	cy.fixture('bank.json').then((testData) => {
+		cy.visit(testData.url);
+	});
+});
+
+When('I submit login with {string} and {string}', (username, password) => {
+	cy.get('#login_form').within(() => {
+		loginPage.loginAs(username, password);
+	});
+});
+
+Then('I should see homepage', () => {
+	mainPage.verifyIfBasicElementsAreDisplayed();
+});
