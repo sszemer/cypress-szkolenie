720af734aa2b17470de34a328708adc43f8d2cb4
sebastian-szemer <sebastian.szemer@inetum.com>
2022-10-21 18:35:15 +0200

    Revert "Revert "refactor""
    This reverts commit 9bb82dd88bb3b5f860f829a488524e157a772d78.

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,44 +1,46 @@
-import { defineConfig } from 'cypress';
+import {defineConfig} from 'cypress';
+import {addCucumberPreprocessorPlugin} from "@badeball/cypress-cucumber-preprocessor";
+import {createEsbuildPlugin} from "@badeball/cypress-cucumber-preprocessor/esbuild";
+
 const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
-const addCucumberPreprocessorPlugin =
-	require('@badeball/cypress-cucumber-preprocessor').addCucumberPreprocessorPlugin;
-const createEsbuildPlugin =
-	require('@badeball/cypress-cucumber-preprocessor/esbuild').createEsbuildPlugin;
 
 export default defineConfig({
-	retries: {
-		runMode: 1,
-		openMode: 0,
-	},
-	video: false,
-	videoCompression: 33,
-	defaultCommandTimeout: 5000,
-	chromeWebSecurity: false,
-	watchForFileChanges: false,
-	viewportWidth: 1280,
-	viewportHeight: 720,
-	execTimeout: 60000,
-	pageLoadTimeout: 60000,
-	requestTimeout: 15000,
-	responseTimeout: 15000,
-	reporter: 'mochawesome',
-	reporterOptions: {
-		reportDir: 'cypress/results',
-		overwrite: false,
-		html: false,
-		json: true,
-	},
-	e2e: {
-		async setupNodeEvents(on, config) {
-			const bundler = createBundler({
-				plugins: [createEsbuildPlugin(config)],
-			});
-
-			on('file:preprocessor', bundler);
-			await addCucumberPreprocessorPlugin(on, config);
-
-			return config;
-		},
-		specPattern: 'cypress/e2e/features/*.feature',
-	},
+    retries: {
+        runMode: 1,
+        openMode: 0,
+    },
+    video: false,
+    videoCompression: 33,
+    defaultCommandTimeout: 5000,
+    chromeWebSecurity: false,
+    watchForFileChanges: false,
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    execTimeout: 60000,
+    pageLoadTimeout: 60000,
+    requestTimeout: 15000,
+    responseTimeout: 15000,
+    reporter: 'mochawesome',
+    reporterOptions: {
+        reportDir: 'cypress/results',
+        overwrite: false,
+        html: false,
+        json: true,
+    },
+    e2e: {
+        setupNodeEvents,
+        specPattern: 'cypress/e2e/features/*.feature',
+    },
 });
+
+async function setupNodeEvents(
+    on: Cypress.PluginEvents,
+    config: Cypress.PluginConfigOptions
+): Promise<Cypress.PluginConfigOptions> {
+    await addCucumberPreprocessorPlugin(on, config);
+    on('file:preprocessor', createBundler({
+        plugins: [createEsbuildPlugin(config)],
+    }));
+
+    return config;
+}
